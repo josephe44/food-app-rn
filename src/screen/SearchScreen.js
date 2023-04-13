5814e680eb2dee86179d3c6ec27a4c3b0c514520
josephe44 <josephe442@gmail.com>
2023-04-13 10:09:27 +0100

    yepl api call logic on search screen

--- src/screen/SearchScreen.js
+++ src/screen/SearchScreen.js
@@ -1,19 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  // call the searchApi function when the component is first rendered
+  useEffect(() => {
+    searchApi("pasta");
+  }, []);
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const res = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "san jose",
+        },
+      });
+      setResults(res.data.businesses);
+    } catch (error) {
+      //  alert the user the error
+      setErrorMessage("Something went wrong");
+    }
+  };
 
   return (
     <View>
       <SearchBar
         term={term}
         onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => {}}
+        onTermSubmit={() => searchApi(term)}
       />
-      <Text>Search Bar</Text>
-      <Text>{term}</Text>
+
+      {errorMessage ? null : <Text>{errorMessage}</Text>}
+      <Text>we have found {results.length} results</Text>
     </View>
   );
 };
